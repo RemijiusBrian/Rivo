c5cb879d2d232091a3fc9b0bedbe87e4a472d8b4
Ridill <ridillperson@gmail.com>
2023-08-07 20:26:18 +0530

    Reverted navigation compose library

--- app/src/main/java/dev/ridill/mym/core/ui/navigation/NavHost.kt
+++ app/src/main/java/dev/ridill/mym/core/ui/navigation/NavHost.kt
@@ -2,8 +2,6 @@ package dev.ridill.mym.core.ui.navigation
 
 import android.Manifest
 import android.os.Build
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -17,8 +15,6 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import dev.ridill.mym.BuildConfig
 import dev.ridill.mym.R
-import dev.ridill.mym.core.ui.components.defaultFadeIn
-import dev.ridill.mym.core.ui.components.defaultFadeOut
 import dev.ridill.mym.core.ui.components.rememberPermissionLauncher
 import dev.ridill.mym.core.ui.components.rememberPermissionsState
 import dev.ridill.mym.core.ui.components.rememberSnackbarHostState
@@ -70,8 +66,8 @@ fun MYMNavHost(
 private fun NavGraphBuilder.welcomeFlow(navController: NavHostController) {
     composable(
         route = WelcomeFlowDestination.route,
-        enterTransition = { defaultFadeIn() },
-        exitTransition = { defaultFadeOut() }
+//        enterTransition = { defaultFadeIn() },
+//        exitTransition = { defaultFadeOut() }
     ) { navBackStackEntry ->
         val viewModel: WelcomeFlowViewModel = hiltViewModel(navBackStackEntry)
         val flowStop by viewModel.currentFlowStop.collectAsStateWithLifecycle()
@@ -129,8 +125,8 @@ private fun NavGraphBuilder.welcomeFlow(navController: NavHostController) {
 private fun NavGraphBuilder.dashboard(navController: NavHostController) {
     composable(
         route = DashboardDestination.route,
-        exitTransition = { defaultFadeOut() },
-        popEnterTransition = { defaultFadeIn() }
+//        exitTransition = { defaultFadeOut() },
+//        popEnterTransition = { defaultFadeIn() }
     ) { navBackStackEntry ->
         val viewModel: DashboardViewModel = hiltViewModel(navBackStackEntry)
         val state by viewModel.state.collectAsStateWithLifecycle()
@@ -172,9 +168,9 @@ private fun NavGraphBuilder.addEditExpense(navController: NavHostController) {
     composable(
         route = AddEditExpenseDestination.route,
         arguments = AddEditExpenseDestination.arguments,
-        enterTransition = { slideInVertically { it } },
-        popExitTransition = { slideOutVertically { it } },
         deepLinks = AddEditExpenseDestination.deepLinks
+//        enterTransition = { slideInVertically { it } },
+//        popExitTransition = { slideOutVertically { it } },
     ) { navBackStackEntry ->
         val viewModel: AddEditExpenseViewModel = hiltViewModel(navBackStackEntry)
         val amount = viewModel.amountInput.collectAsStateWithLifecycle(initialValue = "")
@@ -240,8 +236,8 @@ private fun NavGraphBuilder.addEditExpense(navController: NavHostController) {
 private fun NavGraphBuilder.allExpenses(navController: NavHostController) {
     composable(
         route = AllExpensesDestination.route,
-        enterTransition = { defaultFadeIn() },
-        popExitTransition = { defaultFadeOut() }
+//        enterTransition = { defaultFadeIn() },
+//        popExitTransition = { defaultFadeOut() }
     ) { navBackStackEntry ->
         val viewModel: AllExpensesViewModel = hiltViewModel(navBackStackEntry)
         val state by viewModel.state.collectAsStateWithLifecycle()
@@ -279,8 +275,8 @@ private fun NavGraphBuilder.allExpenses(navController: NavHostController) {
 private fun NavGraphBuilder.settings(navController: NavHostController) {
     composable(
         route = SettingsDestination.route,
-        enterTransition = { defaultFadeIn() },
-        popExitTransition = { defaultFadeOut() }
+//        enterTransition = { defaultFadeIn() },
+//        popExitTransition = { defaultFadeOut() }
     ) { navBackStackEntry ->
         val viewModel: SettingsViewModel = hiltViewModel(navBackStackEntry)
         val state by viewModel.state.collectAsStateWithLifecycle()
