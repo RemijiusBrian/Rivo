9232bab6e2a47cc932939a3fcf1eaa9339671436
Ridill <ridillperson@gmail.com>
2023-08-11 16:43:01 +0530

    Started experimentation on google drive backup

---
+++ app/src/main/java/dev/ridill/mym/core/domain/service/BackupService.kt
@@ -0,0 +1,11 @@
+package dev.ridill.mym.core.domain.service
+
+import android.content.Context
+import dev.ridill.mym.core.data.db.MYMDatabase
+
+class BackupService(
+    private val context: Context,
+    private val database: MYMDatabase
+) {
+
+}
