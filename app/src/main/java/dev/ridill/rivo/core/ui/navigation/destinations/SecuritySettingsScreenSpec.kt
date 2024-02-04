3d897614033e4d1b3e1a6fa43a6670d74f0cb55e
Ridill <ridillperson@gmail.com>
2024-02-04 18:00:31 +0400

    Add navigation anim

--- app/src/main/java/dev/ridill/rivo/core/ui/navigation/destinations/SecuritySettingsScreenSpec.kt
+++ app/src/main/java/dev/ridill/rivo/core/ui/navigation/destinations/SecuritySettingsScreenSpec.kt
@@ -1,5 +1,8 @@
 package dev.ridill.rivo.core.ui.navigation.destinations
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -9,6 +12,8 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
 import dev.ridill.rivo.R
+import dev.ridill.rivo.core.ui.components.slideInHorizontallyWithFadeIn
+import dev.ridill.rivo.core.ui.components.slideOutHorizontallyWithFadeOut
 import dev.ridill.rivo.settings.presentation.security.SecuritySettingsScreen
 import dev.ridill.rivo.settings.presentation.security.SecuritySettingsViewModel
 import dev.ridill.rivo.settings.presentation.settings.rememberBiometricPrompt
@@ -18,6 +23,12 @@ data object SecuritySettingsScreenSpec : ScreenSpec {
     override val route: String = "security_settings"
 
     override val labelRes: Int = R.string.destination_security_settings
+
+    override val popExitTransition: AnimatedContentTransitionScope<NavBackStackEntry>.() -> ExitTransition? =
+        { slideOutHorizontallyWithFadeOut { it } }
+
+    override val enterTransition: AnimatedContentTransitionScope<NavBackStackEntry>.() -> EnterTransition? =
+        { slideInHorizontallyWithFadeIn { it } }
 
     @Composable
     override fun Content(navController: NavHostController, navBackStackEntry: NavBackStackEntry) {
