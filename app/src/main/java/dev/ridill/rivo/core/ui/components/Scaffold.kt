946f9e8d10a1a9ff5f11d597b3e71ca69d374ee9
Ridill <ridillperson@gmail.com>
2024-03-08 14:42:24 +0400

    Cleanup

--- app/src/main/java/dev/ridill/rivo/core/ui/components/Scaffold.kt
+++ app/src/main/java/dev/ridill/rivo/core/ui/components/Scaffold.kt
@@ -1,23 +1,15 @@
 package dev.ridill.rivo.core.ui.components
 
-import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.material3.BottomSheetDefaults
-import androidx.compose.material3.BottomSheetScaffold
-import androidx.compose.material3.BottomSheetScaffoldState
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.ScaffoldDefaults
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.contentColorFor
-import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.unit.Dp
 
 @Composable
 fun RivoScaffold(
@@ -42,45 +34,5 @@ fun RivoScaffold(
     containerColor = containerColor,
     contentColor = contentColor,
     contentWindowInsets = contentWindowInsets,
-    content = content
-)
-
-@Composable
-fun RivoBottomSheetScaffold(
-    sheetContent: @Composable ColumnScope.() -> Unit,
-    modifier: Modifier = Modifier,
-    snackbarController: SnackbarController = rememberSnackbarController(),
-    scaffoldState: BottomSheetScaffoldState = rememberBottomSheetScaffoldState(snackbarHostState = snackbarController.snackbarHostState),
-    sheetPeekHeight: Dp = BottomSheetDefaults.SheetPeekHeight,
-    sheetMaxWidth: Dp = BottomSheetDefaults.SheetMaxWidth,
-    sheetShape: Shape = BottomSheetDefaults.ExpandedShape,
-    sheetContainerColor: Color = BottomSheetDefaults.ContainerColor,
-    sheetContentColor: Color = contentColorFor(sheetContainerColor),
-    sheetTonalElevation: Dp = BottomSheetDefaults.Elevation,
-    sheetShadowElevation: Dp = BottomSheetDefaults.Elevation,
-    sheetDragHandle: @Composable (() -> Unit)? = { BottomSheetDefaults.DragHandle() },
-    sheetSwipeEnabled: Boolean = true,
-    topBar: @Composable () -> Unit = {},
-    snackbarHost: @Composable (SnackbarHostState) -> Unit = { RivoSnackbarHost(it) },
-    containerColor: Color = MaterialTheme.colorScheme.background,
-    contentColor: Color = contentColorFor(containerColor),
-    content: @Composable (PaddingValues) -> Unit
-) = BottomSheetScaffold(
-    sheetContent = sheetContent,
-    modifier = modifier,
-    scaffoldState = scaffoldState,
-    sheetPeekHeight = sheetPeekHeight,
-    sheetMaxWidth = sheetMaxWidth,
-    sheetShape = sheetShape,
-    sheetContainerColor = sheetContainerColor,
-    sheetContentColor = sheetContentColor,
-    sheetTonalElevation = sheetTonalElevation,
-    sheetShadowElevation = sheetShadowElevation,
-    sheetDragHandle = sheetDragHandle,
-    sheetSwipeEnabled = sheetSwipeEnabled,
-    topBar = topBar,
-    snackbarHost = snackbarHost,
-    containerColor = containerColor,
-    contentColor = contentColor,
     content = content
 )
