1d004792e4857eeac9260b3a874df095146a2157
Ridill <ridillperson@gmail.com>
2024-03-26 11:09:03 +0530

    Add check to ignore future timestamp

--- app/src/main/java/dev/ridill/rivo/transactions/domain/autoDetection/TransactionAutoDetectService.kt
+++ app/src/main/java/dev/ridill/rivo/transactions/domain/autoDetection/TransactionAutoDetectService.kt
@@ -2,6 +2,7 @@ package dev.ridill.rivo.transactions.domain.autoDetection
 
 import android.telephony.SmsMessage
 import dev.ridill.rivo.core.domain.notification.NotificationHelper
+import dev.ridill.rivo.core.domain.util.DateUtil
 import dev.ridill.rivo.core.domain.util.tryOrNull
 import dev.ridill.rivo.di.ApplicationScope
 import dev.ridill.rivo.transactions.domain.model.Transaction
@@ -18,9 +19,12 @@ class TransactionAutoDetectService(
     fun detectTransactionsFromMessages(messages: List<SmsMessage>) = applicationScope.launch {
         val orgRegex = ORG_ADDRESS_PATTERN.toRegex()
         val messagesFromOrg = messages.filter { orgRegex.matches(it.displayMessageBody.orEmpty()) }
+        val dateTimeNow = DateUtil.now()
         for (message in messagesFromOrg) {
             val data = tryOrNull { extractor.extractData(message.messageBody) }
                 ?: continue
+
+            if (data.paymentTimestamp.isAfter(dateTimeNow)) continue
 
             val insertedTx = transactionRepo.saveTransaction(
                 amount = data.amount,
