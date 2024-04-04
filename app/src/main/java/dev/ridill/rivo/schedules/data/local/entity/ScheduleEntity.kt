88a2470f7bcc522386683d85c7d5feae05089038
Ridill <ridillperson+github@gmail.com>
2024-04-04 20:38:03 +0530

    Add default value

--- app/src/main/java/dev/ridill/rivo/schedules/data/local/entity/ScheduleEntity.kt
+++ app/src/main/java/dev/ridill/rivo/schedules/data/local/entity/ScheduleEntity.kt
@@ -33,6 +33,6 @@ data class ScheduleEntity(
     @ColumnInfo(name = "next_reminder_date")
     val nextReminderDate: LocalDate?,
 
-    @ColumnInfo(name = "last_paid_date")
+    @ColumnInfo(name = "last_paid_date", defaultValue = "NULL")
     val lastPaidDate: LocalDate?
 )
