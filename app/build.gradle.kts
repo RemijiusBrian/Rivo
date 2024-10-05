fba5301404b5354f4805303989b090901cc2d3ed
Ridill <ridillperson@gmail.com>
2024-10-05 23:13:05 +0530

    Target SDK upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,12 +15,12 @@ plugins {
 
 android {
     namespace = "dev.ridill.rivo"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "dev.ridill.rivo"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "0.0.1"
 
