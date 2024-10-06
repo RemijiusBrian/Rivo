249f925fba3be7f726269713388ab2000c56b32e
Ridill <ridillperson@gmail.com>
2024-10-06 12:32:31 +0530

    Cleanup ConfigDao

--- app/src/main/java/dev/ridill/rivo/settings/data/local/ConfigDao.kt
+++ app/src/main/java/dev/ridill/rivo/settings/data/local/ConfigDao.kt
@@ -4,13 +4,9 @@ import androidx.room.Dao
 import androidx.room.Query
 import dev.ridill.rivo.core.data.db.BaseDao
 import dev.ridill.rivo.settings.data.local.entity.ConfigEntity
-import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ConfigDao : BaseDao<ConfigEntity> {
     @Query("SELECT config_value FROM config_table WHERE config_key = '${ConfigKeys.BACKUP_INTERVAL}'")
     suspend fun getBackupInterval(): String?
-
-    @Query("SELECT config_value FROM config_table WHERE config_key = '${ConfigKeys.FOLDERS_LIST_MODE}'")
-    fun getFoldersListMode(): Flow<String?>
 }
