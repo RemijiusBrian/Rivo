bf05185b2bae98324f4e2734ddefec8c39e55838
Ridill <ridillperson+github@gmail.com>
2024-10-20 10:15:32 +0530

    Refactor

--- app/src/main/java/dev/ridill/rivo/transactions/domain/model/TransactionDateLimits.kt
+++ app/src/main/java/dev/ridill/rivo/transactions/domain/model/TransactionDateLimits.kt
@@ -3,6 +3,6 @@ package dev.ridill.rivo.transactions.domain.model
 import java.time.LocalDate
 
 data class TransactionDateLimits(
-    val minDate: LocalDate,
-    val maxDate: LocalDate
+    val maxDate: LocalDate,
+    val minDate: LocalDate
 )
